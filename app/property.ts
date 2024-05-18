71f2a16457f3d4a9767a0669ab07083373bb2528
gayeon-kaho <106268292+gayeon-kaho@users.noreply.github.com>
2024-05-18 21:35:20 +0900

    Update property.ts

--- app/property.ts
+++ app/property.ts
@@ -2,3 +2,6 @@ export const yourName = "Lee GaYeon (KaHo)";
 export const gmailLink = "gayeon8791@gmail.com"
 export const linkedinLink = "https://www.linkedin.com/in/gayeon-lee-4b335123b/"
 export const githubLink = "https://github.com/gayeon-kaho"
+export const orizontlink = "https://orizont.md/acasa/"
+export const ogatalablink = "http://www.falcon.cs.kumamoto-u.ac.jp/"
+export const uranishilablink = "https://www.lab.ime.cmc.osaka-u.ac.jp/"
