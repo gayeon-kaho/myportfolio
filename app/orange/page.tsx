5df09e30c336495d425597b759393e31868c63fb
gayeon-kaho <106268292+gayeon-kaho@users.noreply.github.com>
2024-05-18 15:32:44 +0900

    Update page.tsx

--- app/orange/page.tsx
+++ app/orange/page.tsx
@@ -1,6 +1,6 @@
 import Header from "@/app/orange/_components/header";
 import Footer from "@/app/orange/_components/footer";
-import {githubLink, LinkedInLink, twitterLink, yourName} from "@/app/property";
+import {githubLink, linkedinLink, twitterLink, yourName} from "@/app/property";
 import "@/app/orange/_components/header.css";
 import Image from "next/image";
 import Fruit from "@/assets/orange/fruit.svg"
@@ -45,9 +45,9 @@ export default function Page() {
                                    className={'inline'}>Twitter</a>
                             </li>
                             <li>
-                                <Image src={Instagram} alt={'linkedin'}
+                                <Image src={LinkedIn} alt={'linkedin'}
                                        width={30} height={30} className={'inline mr-3'}/>
-                                <a href={LinkedInLink} target={'_blank'} rel={'noopener noreferrer'}
+                                <a href={linkedinLink} target={'_blank'} rel={'noopener noreferrer'}
                                    className={'inline'}>LinkedIn</a>
                             </li>
                             <li>
