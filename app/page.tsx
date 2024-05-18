23b76648225a61360cb9839aeebe74b73c7cd6a0
gayeon-kaho <106268292+gayeon-kaho@users.noreply.github.com>
2024-05-18 17:06:34 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,29 @@ export default function Home() {
           <Link href={'/photographic'}>
               Photographic
           </Link>
+                    <section className={'bg-orange-container-a rounded-[3rem] p-12 leading-loose ' +
+                        'absolute right-12 top-12'}>
+                        <ul className={'flex flex-col gap-4'}>
+                            <li>
+                                <Image src={Gmail} alt={'gmail'}
+                                       width={30} height={30} className={'inline mr-3'}/>
+                                <a href={gmailLink} target={'_blank'} rel={'noopener noreferrer'}
+                                   className={'inline'}>Gmail</a>
+                            </li>
+                            <li>
+                                <Image src={LinkedIn} alt={'linkedin'}
+                                       width={30} height={30} className={'inline mr-3'}/>
+                                <a href={linkedinLink} target={'_blank'} rel={'noopener noreferrer'}
+                                   className={'inline'}>LinkedIn</a>
+                            </li>
+                            <li>
+                                <Image src={GitHub} alt={'github'}
+                                       width={30} height={30} className={'inline mr-3'}/>
+                                <a href={githubLink} target={'_blank'} rel={'noopener noreferrer'}
+                                   className={'inline'}>GitHub</a>
+                            </li>
+                        </ul>
+                    </section>
       </main>
   );
 }
