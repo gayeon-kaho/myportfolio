191598d74573786af223d1c8e2a9c2bbc532c2d6
gayeon-kaho <106268292+gayeon-kaho@users.noreply.github.com>
2024-05-18 15:31:35 +0900

    Update property.ts

--- app/property.ts
+++ app/property.ts
@@ -1,4 +1,4 @@
 export const yourName = "Lee GaYeon (KaHo)";
 export const twitterLink = "https://twitter.com/"
-export const LinkedInLink = "www.linkedin.com/in/gayeon-lee-4b335123b"
+export const linkedinLink = "www.linkedin.com/in/gayeon-lee-4b335123b"
 export const githubLink = "https://github.com/gayeon-kaho"
