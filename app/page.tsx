8a4fec2613699266a7f4c662160566728b56c4c7
gayeon-kaho <106268292+gayeon-kaho@users.noreply.github.com>
2024-05-18 20:57:04 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import Header from "@/app/orange/_components/header";
 import Footer from "@/app/orange/_components/footer";
-import {githubLink, linkedinLink, gmailLink, yourName} from "@/app/property";
+import {githubLink, linkedinLink} from "@/app/property";
 import Image from "next/image";
 import LinkedIn from "@/assets/linkedin_logo.png"
 import GitHub from "@/assets/github_logo.png"
-import Gmail from"@/assets/gmail_logo.png"
 import myhistory from"@/assets/myhistory.png"
 
 export default function Home() {
@@ -13,18 +12,12 @@ export default function Home() {
           <div className={'max-w-screen-lg flex flex-col items-center mx-auto px-12 z-10'}>
             <Header/>
             <h1 className={"py-32 line"}>
-              <p className={'inline text-6xl font-semibold pr-6'}>{yourName}</p>
+              <p className={'inline text-6xl font-semibold pr-6'}>Hello, I'm GaYeon/KaHo!</p>
             </h1>
             <div className={'relative w-full  h-[36rem]'}>
-              <section className={'bg-orange-container-a rounded-[3rem] p-12 leading-loose ' +
-                        'absolute right-12 top-12'}>
+              <section className={'bg-orange-container-a rounded-[1rem] p-12 leading-loose ' +
+                        'absolute top-12'} style={{ left: '-17rem' }}>
                 <ul className={'flex flex-col gap-4'}>
-                  <li>
-                    <Image src={Gmail} alt={'gmail'}
-                      width={30} height={30} className={'inline mr-3'}/>
-                    <a href={gmailLink} target={'_blank'} rel={'noopener noreferrer'}
-                      className={'inline'}>Gmail</a>
-                  </li>
                   <li>
                     <Image src={LinkedIn} alt={'linkedin'}
                       width={30} height={30} className={'inline mr-3'}/>
@@ -41,7 +34,7 @@ export default function Home() {
               </section>
             </div>
             <div>
-              <Image src={myhistory} alt={"myhistory"}  width={1600} height={1200} className={'items-center absolute top-[800px] right-0'}/>
+              <Image src={myhistory} alt={"myhistory"}  width={1600} height={1200} className={'items-center absolute top-[720px] right-0'}/>
             </div>
             <Footer/>
           </div>
