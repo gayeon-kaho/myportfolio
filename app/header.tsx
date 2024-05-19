8ca083c5bc84030c94fd6c139236829249fdaa7d
gayeon-kaho <106268292+gayeon-kaho@users.noreply.github.com>
2024-05-19 10:00:12 +0900

    Add files via upload

---
+++ app/header.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Image from "next/image";
+import Triangle from "@/assets/orange/triangle.svg"
+
+export default function Header() {
+    return (
+        <header className={'flex flex-row items-center gap-6'}>
+            <Link href={'/photographic'}>I've Done</Link>
+            <Image src={Triangle} alt={"fruit"}  width={19} height={21}/>
+            <Link href={'/paper'}>I've Learned</Link>
+            <Image src={Triangle} alt={"fruit"}  width={19} height={21}/>
+            <Link href={'/orange'}>I Love</Link>
+        </header>
+    )
+}
