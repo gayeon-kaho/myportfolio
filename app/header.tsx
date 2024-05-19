714f0fc44862d83b2ae21a061949f6f44a0c28fb
gayeon-kaho <106268292+gayeon-kaho@users.noreply.github.com>
2024-05-19 20:07:42 +0900

    Update header.tsx

--- app/header.tsx
+++ app/header.tsx
@@ -5,9 +5,9 @@ import Triangle from "@/assets/orange/triangle.svg"
 export default function Header() {
     return (
         <header className={'flex flex-row items-center gap-6'}>
-            <Link href={'/photographic'}>I've Done</Link>
+            <Link href={'/photographic'}>I&apos;ve Done</Link>
             <Image src={Triangle} alt={"fruit"}  width={19} height={21}/>
-            <Link href={'/paper'}>I've Learned</Link>
+            <Link href={'/paper'}>I&apos;ve Learned</Link>
             <Image src={Triangle} alt={"fruit"}  width={19} height={21}/>
             <Link href={'/orange'}>I Love</Link>
         </header>
