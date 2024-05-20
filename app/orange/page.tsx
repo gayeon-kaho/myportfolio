a8643c654c4fb78b12b7327110dbbafe17657734
gayeon-kaho <106268292+gayeon-kaho@users.noreply.github.com>
2024-05-20 21:45:58 +0900

    Update page.tsx

--- app/orange/page.tsx
+++ app/orange/page.tsx
@@ -1,11 +1,10 @@
 import "@/app/photographic/photographic.css"
 import Image from "next/image";
-import CherryBlossom from "@/assets/photographic/cherry.png";
-import Nemophila from "@/assets/photographic/nemophila.jpg";
 import Jazz1 from "@/assets/orange/jazz1.png";
 import Jazz2 from "@/assets/orange/jazz2.png";
 import Food1 from "@/assets/food1.png";
 import Food2 from "@/assets/food2.png";
+import People from "@/assets/orange/ppl.png";
 
 export default function Page() {
     return (
@@ -28,12 +27,11 @@ export default function Page() {
                     <p>I love to cook, bake and eat since I was 8 years old</p>
                 </section>
                 <Image src={Food1} alt={'food1'} className={'w-full h-96 object-cover ml-auto'}/>
-                <Image src={CherryBlossom} alt={'cherry-blossom'} className={'w-8/12 h-96 object-cover'}/>
                 <section className={'bg-photo-surface h-48 flex flex-col justify-center items-center gap-2'}>
                     <h2 className={'font-semibold'}>People</h2>
                     <p>When people being people!</p>
                 </section>
-                <Image src={Nemophila} alt={'nemophila'} className={'w-8/12 h-96 object-cover ml-auto'}/>
+                <Image src={People} alt={'people'} className={'w-full h-96 object-cover ml-auto'}/>
             </main>
             <footer className={'py-8'}>
                 Lee GaYeon (KaHo) © {new Date().getFullYear()}
