4cd965c152aa19dda041619b0909b740ff5acbc8
gayeon-kaho <106268292+gayeon-kaho@users.noreply.github.com>
2024-05-20 18:07:53 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
       <main className={'w-screen h-screen p-20 bg-slate-50 flex flex-row gap-4'}>
         <div className={'relative'}>
-                    <Image src={thumbnail} alt={"thumbnail"} width={960} height={1440} className={'absolute top-12 left-16'}/>
+                    <Image src={thumbnail} alt={"thumbnail"} width={480} height={720} className={'absolute top-12 left-16'}/>
           <div className={'max-w-screen-lg flex flex-col items-center mx-auto px-12 z-10'}>
             <Header/>
             <h1 className={"py-32 line"}>
