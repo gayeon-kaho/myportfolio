f9a996b17e49dbbec32e5b3e1766841770afd240
gayeon-kaho <106268292+gayeon-kaho@users.noreply.github.com>
2024-05-20 19:21:02 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,16 +10,16 @@ import thumbnail from "@/assets/thumbnail.jpg"
 export default function Home() {
   return (
       <main className={'w-screen h-screen p-20 bg-slate-50 flex flex-row gap-4'}>
-        <div className={'relative'}>
-                    <Image src={thumbnail} alt={"thumbnail"} width={360} height={540} className={'absolute top-12 left-8 -z-10'}/>
           <div className={'max-w-screen-lg flex flex-col items-center mx-auto px-12 z-10'}>
             <Header/>
             <h1 className={"py-32 line"}>
               <p className={'inline text-6xl font-semibold pr-6'}>Hello, I&apos;m GaYeon/KaHo!</p>
             </h1>
+            <div>
+              <Image src={thumbnail} alt={"thumbnail"} width={360} height={540} className={'absolute top-36 left-8 transform  -z-10'}/>
             <div className={'relative w-full  h-[36rem]'}>
               <section className={'bg-orange-container-a rounded-[1rem] p-12 leading-loose ' +
-                        'absolute top-12'} style={{ right: '-16rem' }}>
+                        'absolute top-12'} style={{ right: '-32rem' }}>
                 <ul className={'flex flex-col gap-4'}>
                   <li>
                     <Image src={LinkedIn} alt={'linkedin'}
