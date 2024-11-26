b84ede6dc5f29a44fccaafdfc995e1bd5f4b8dee
gayeon-kaho <106268292+gayeon-kaho@users.noreply.github.com>
2024-11-26 14:35:34 +0900

    Update property.ts
    changed linked in link

--- app/property.ts
+++ app/property.ts
@@ -1,6 +1,6 @@
 export const yourName = "Lee GaYeon (KaHo)";
 export const gmailLink = "gayeon8791@gmail.com"
-export const linkedinLink = "https://www.linkedin.com/in/gayeon-lee-4b335123b/"
+export const linkedinLink = "https://www.linkedin.com/in/gayeon-lee-kaho"
 export const githubLink = "https://github.com/gayeon-kaho"
 export const orizontlink = "https://orizont.md/acasa/"
 export const ogatalablink = "http://www.falcon.cs.kumamoto-u.ac.jp/"
